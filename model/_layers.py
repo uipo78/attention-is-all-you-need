75c96253af18e873af60663851ff9ae55616fc79
uipo78 <brandencarrier@gmail.com>
2017-12-03 17:23:01 -0600

    Add progress

---
+++ model/_layers.py
@@ -0,0 +1,83 @@
+import torch.nn as nn
+
+from ._sublayers import PositionWiseFFN, MultiHeadAttention
+
+
+class Encoder(nn.Module):
+    """docstring for Encoder."""
+    # TODO: figure out dimensions
+
+    def __init__(self, n_layers=6):
+        super().__init__()
+
+        self.embedding = nn.Embedding()
+        self.pe = PositionalEncoding()
+        self.stack = nn.ModuleList([_EncoderLayer()] * n_layers)
+
+    def forward(self, x):
+        x = self.pe(x)
+        for layer in self.stack:
+            pass
+
+        return x
+
+
+class _EncoderLayer(nn.Module):
+    """docstring for _EncoderLayer."""
+    # TODO: figure out dimensions
+
+    def __init__(self):
+        super().__init__()
+
+        self.multihead = MultiHeadAttention()
+        self.pw_ffn = PositionWiseFFN()
+        self.norm = pass
+
+    def forward(self, x):
+        x = self.norm(x + self.multihead(x))
+        x = self.norm(x + self.pw_ffn(x))
+
+        return x
+
+
+class Decoder(nn.Module):
+    """docstring for Decoder."""
+    # TODO: figure out dimensions
+
+    def __init__(self, n_layers=6):
+        super().__init__()
+
+        self.embedding = nn.Embedding()
+        self.pe = PositionalEncoding()
+        self.stack = nn.ModuleList([_EncoderLayer()] * n_layers)
+
+    def forward(self, x):
+        x = self.pe(x)
+        for layer in self.stack:
+            pass
+
+        return x
+
+
+class _DecoderLayer(nn.Module):
+    """docstring for _DecoderLayer."""
+    # TODO: figure out dimensions
+
+    def __init__(self):
+        super().__init__()
+
+        self.masked_multihead = MultiHeadAttention()
+        self.multihead = MultiHeadAttention()
+        self.pw_ffn = PositionWiseFFN()
+        self.norm = pass
+
+    def forward(self, x, x_encoder):
+        x = self.norm(x + self.masked_multihead(x))
+        x = self.norm(x + self.multhead(x, x_encoder))
+        x = self.norm(x + self.pw_ffn(x))
+
+        return x
+
+
+class _PositionalEncoding(nn.Module):
+    pass
---
+++ model/_sublayers.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+if torch.cuda.is_available():
+    FloatTensor = torch.cuda.FloatTensor
+else:
+    FloatTensor = torch.FloatTensor
+
+
+class PositionWiseFFN(nn.Module):
+    """docstring for PositionWiseFFN."""
+    # TODO: figure out dimensions
+
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Linear()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    """docstring for MultiHeadAttention."""
+
+    def __init__(self, d_model=512, h=8, p=None, mask=None):
+        super().__init__()
+
+        # See top of page 5
+        d_k = d_v = d_model // h
+
+        self.layer_norm = layer_norm
+
+        # Rather than constructing a set of W matrices for Q, K, and V,
+        # we instead stack all such matrices, resulting in an order 3 tensor.
+        self.W_Q = nn.Parameters(Variable(FloatTensor(h, d_model, d_k)))
+        self.W_K = nn.Parameters(Variable(FloatTensor(h, d_model, d_k)))
+        self.W_V = nn.Parameters(Variable(FloatTensor(h, d_model, d_v)))
+        self.W_O = nn.Parameters(Variable(FloatTensor(h * d_v, d_model)))
+
+        self.attention = _ScaledDotProductAttention(p, mask)
+
+    def forward(self, Q, K, V):
+        # Q, K, and V are of dimension (length {Q, K, V}, d_model)
+        QW, KW, VW = Q.matmul(self.W_Q), K.matmul(self.W_K), K.matmul(self.W_K)
+
+        # h collection in figure 2
+        heads = self.attention(QW, KW, VW)
+
+        # Concat step in figure 2
+        batch_size, h, Q_len, d_v = heads.size()
+        concat = heads.view(batch_size, Q_len, h * d_v)
+
+        # Linear step in figure 2
+        x = concat.bmm(self.W_O)
+
+        return x
+
+
+class _ScaledDotProductAttention(nn.Module):
+    """docstring for _ScaledDotProductAttention."""
+
+    def __init__(self, p=None, mask=None):
+        super().__init__()
+        self.p = p
+        self.mask = mask
+
+    def forward(self, Q, K, V):
+        # MatMul and Scale steps in figure 2
+        x = Q.matmul(K.transpose(-2, -1)) / K.size(-1) ** 0.5
+
+        # Optional masking layer
+        if self.mask is not None:
+            pass
+
+        # Softmax step in figure 2
+        *dims, d_k = x.size()
+        # We need to reshape x so that we can apply softmax across the row of
+        # each h-slice of each batch
+        x = F.softmax(x.view(-1, d_k))
+        x = x.view(*dims, d_k)
+
+        # Optional dropout regularization
+        if self.p is not None:
+            x = F.dropout(x, p=p)
+
+        return x.matmul(V)
