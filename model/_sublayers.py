f8d65b142000c60b29eefaa5921bea49aa41d369
Branden Carrier <brandencarrier@gmail.com>
2017-12-04 08:45:18 -0600

    Add to PositionalEncoding

--- model/_sublayers.py
+++ model/_sublayers.py
@@ -11,7 +11,28 @@ else:
 
 
 class PositionalEncoding(nn.Module):
-    pass
+
+    def __init__(self, learned, vocab_size, d_model):
+
+        super().__init__()
+        self.embedding = Embedding(vocab_size, d_model)
+        if learned:
+            pass
+        else:
+            self.embedding.weight.data = self._init_weights(vocab_size, d_model)
+
+    @staticmethod
+    def _init_weights(vocab_size, d_model):
+        retdata = FloatTensor(vocab_size, d_model).zeros_()
+        for pos in range(1, vocab_size):
+            for i in range(d_model):
+                inner = FloatTensor([pos / (int(10e3)**(2 * i / d_model))])
+                retdata[pos, i] = torch.sin(inner) if i % 2 == 0 else torch.cos(inner)
+
+        return retdata
+
+    def forward(self):
+        pass
 
 
 class MultiHeadAttention(nn.Module):
