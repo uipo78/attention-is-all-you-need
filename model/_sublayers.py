f0d1fa7defd4b2120e0bac736e5cad37d8fd6ad6
Branden Carrier <brandencarrier@gmail.com>
2018-01-01 10:37:40 -0600

    Rename retdata

--- model/_sublayers.py
+++ model/_sublayers.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from torch.autograd import Variable
 
 
@@ -11,25 +12,25 @@ else:
 
 
 class PositionalEncoding(nn.Module):
+    """docstring for PositionalEncoding."""
 
-    def __init__(self, learned, vocab_size, d_model):
-
+    def __init__(self, vocab_size, d_model):
         super().__init__()
         self.embedding = Embedding(vocab_size, d_model)
-        if learned:
-            pass
-        else:
-            self.embedding.weight.data = self._init_weights(vocab_size, d_model)
+        self.embedding.weight.data = self._init_weights(vocab_size, d_model)
 
     @staticmethod
     def _init_weights(vocab_size, d_model):
-        retdata = FloatTensor(vocab_size, d_model).zeros_()
+        weights = FloatTensor(vocab_size, d_model).zeros_()
         for pos in range(1, vocab_size):
             for i in range(d_model):
                 inner = FloatTensor([pos / (int(10e3)**(2 * i / d_model))])
-                retdata[pos, i] = torch.sin(inner) if i % 2 == 0 else torch.cos(inner)
+                if i % 2 == 0:
+                    weights[pos, i] = torch.sin(inner)
+                else:
+                    weights[pos, i] = torch.cos(inner)
 
-        return retdata
+        return weights
 
     def forward(self):
         pass
