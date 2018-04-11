d200d3d0a14fb2554c1d9eca5106103287b14ccd
Branden Carrier <branden.carrier@crowehorwath.com>
2018-04-10 22:17:56 -0500

    Add more to names

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -20,13 +20,13 @@ class Transformer(nn.Module):
 
         if in_vocab_size == out_vocab_size:
             self._shared_weights = True
-            self.io_embedding = nn.Embedding(in_vocab_size, d_model)
+            self.token_embedding = nn.Embedding(in_vocab_size, d_model)
             self.pos_embedding = PositionalEncoding(in_vocab_size, d_model)
         else:
             self._shared_weights = False
-            self.in_embedding = nn.Embedding(in_vocab_size, d_model)
+            self.in_token_embedding = nn.Embedding(in_vocab_size, d_model)
             self.in_pos_embedding = PositionalEncoding(in_vocab_size, d_model)
-            self.out_embedding = nn.Embedding(out_vocab_size, d_model)
+            self.out_token_embedding = nn.Embedding(out_vocab_size, d_model)
             self.out_pos_embedding = PositionalEncoding(out_vocab_size, d_model)
         self.encoder_stack = nn.ModuleList(
             [EncoderLayer(d_model, h, p, d_ff, epsilon)] * n_layers
@@ -34,7 +34,7 @@ class Transformer(nn.Module):
         self.decoder_stack = nn.ModuleList(
             [DecoderLayer(d_model, h, p, d_ff, epsilon)] * n_layers
         )
-        self.fc_softmax = nn.Sequential(
+        self.out = nn.Sequential(
             nn.Linear(d_model, out_vocab_size),
             nn.Softmax()
         )
@@ -43,9 +43,9 @@ class Transformer(nn.Module):
 
         # Input and positional embedding
         if self._shared_weights:
-            x = self.io_embedding(x_seq) + self.pos_embedding(x_pos)
+            x = self.token_embedding(x_seq) + self.pos_embedding(x_pos)
         else:
-            x = self.in_embedding(x_seq) + self.in_pos_embedding(x_pos)
+            x = self.in_token_embedding(x_seq) + self.in_pos_embedding(x_pos)
 
         # Encoder
         mask = self._make_padding_mask(x_seq, x_seq)
@@ -54,16 +54,16 @@ class Transformer(nn.Module):
 
         # Output and positional embedding
         if self._shared_weights:
-            y = self.io_embedding(y_seq) + self.pos_embedding(y_pos)
+            y = self.token_embedding(y_seq) + self.pos_embedding(y_pos)
         else:
-            y = self.out_embedding(y_seq) + self.out_pos_embedding(y_pos)
+            y = self.out_token_embedding(y_seq) + self.out_pos_embedding(y_pos)
 
         # Decoder
         masks = self._make_decoder_masks(x_seq, y_seq)
         for decoder_layer in self.decoder_stack:
             y = decoder_layer(y, x, *masks)
 
-        return self.fc_softmax(y)
+        return self.out(y)
 
     @classmethod
     def _make_decoder_masks(cls, x_seq, y_seq):
