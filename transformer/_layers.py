29be6020d14352f86b83dbc2cafdc12fb765397f
Branden Carrier <branden.carrier@crowehorwath.com>
2018-06-07 08:34:23 -0500

    Fix number of LayerNorms in encoder

--- transformer/_layers.py
+++ transformer/_layers.py
@@ -39,10 +39,11 @@ class EncoderLayer(nn.Module):
         super().__init__()
 
         self.multihead = MultiHeadAttention(d_model=d_model, h=h, p=p)
-        self.norms = nn.ModuleList([
-            LayerNorm(d_hidden=d_model, epsilon=epsilon) for _ in range(3)
-        ])
         self.pw_ffn = PositionWiseFFN(d_model, d_ff, dropout)
+        # # LayerNorms = # MultiHeadAttentions + # PositionWiseFFNs
+        self.norms = nn.ModuleList([
+            LayerNorm(d_hidden=d_model, epsilon=epsilon) for _ in range(2)
+        ])
 
     def forward(self, x, mask):
         x = self.norms[0](x + self.multihead(Q=x, K=x, V=x, mask=mask))
@@ -60,18 +61,17 @@ class DecoderLayer(nn.Module):
         self.attns = nn.ModuleList([
             MultiHeadAttention(d_model=d_model, h=h, p=p) for _ in range(2)
         ])
+        self.pw_ffn = PositionWiseFFN(d_model, d_ff, dropout)
+        # # LayerNorms = # MultiHeadAttentions + # PositionWiseFFNs
         self.norms = nn.ModuleList([
             LayerNorm(d_hidden=d_model, epsilon=epsilon) for _ in range(3)
         ])
-        self.pw_ffn = PositionWiseFFN(d_model, d_ff, dropout)
 
     def forward(self, x, enc, src_mask, target_mask):
         x = self.norms[0](x + self.attns[0](Q=x, K=x, V=x, mask=target_mask))
-
         # To understand the inputs for masked_multihead, look at section 3.2.3
         # of the paper.
         x = self.norms[1](x + self.attns[1](Q=x, K=enc, V=enc, mask=src_mask))
-
         x = self.norms[2](x + self.pw_ffn(x))
 
         return x
